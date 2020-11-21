49dc5b6345f05436e81a398836c5fda1647dc626
Skylar Wong <li.q.wong@gmail.com>
2020-11-20 18:30:17 -0600

    position form inputs and submit button

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -13,24 +13,17 @@ import {
 
 const ContactUs = () => {
     return (  
-    // <Flex direction="column" 
-    //      alignItems="center"  
-    //     justifyContent="flex-start" bg="#302F35" h="100vh">
-    //   <Box border="1px" rounded="5px" mt="5%" mb="5%" ml="20%" mr="20%" pt="2%" pb="2%" pl="5%" pr="5%" textAlign="center" backgroundColor="rgba(216,216,216,0.1)" color="#bcbccc">
-    //     <Text fontSize="lg" color="#ffffff" fontSize="150%">Contact Us</Text>
-    //     <Text fontSize="75%">Send us a message!</Text>
-    //     <Button m="1%" bg="#5766F1" color="#ffffff" w="84px">Submit</Button>
-    //   </Box>
-
-    // </Flex>
     <Box bg="#302F35" h="100vh">
         <Text textAlign="center" fontSize="lg" color="#ffffff" fontSize="150%">Contact Us</Text>
+        <br></br>
         <Box border="1px" rounded="5px" 
             // mt="5%" mb="5%"  
             // margin="auto"
             mx="20%"
-            pt="2%" pb="2%" pl="5%" pr="5%" 
-            textAlign="center"
+            py="2%"
+            px="5%"
+        
+            // textAlign="center"
             color="#bcbccc"
             >
             <form>
@@ -38,20 +31,20 @@ const ContactUs = () => {
                     <FormLabel color="#FFFFFF">Name</FormLabel>
                     <Input border="1px" rounded="5px" 
                         // mt="5%" mb="5%"  
-                        margin="auto"
-                        pt="2%" pb="2%" pl="5%" pr="5%" 
+                        // margin="auto"
+                        // pt="2%" pb="2%" pl="5%" pr="5%" 
                         backgroundColor="rgba(216,216,216,0.1)" color="#bcbccc">
                     
                     </Input>
                 </FormControl>
                 <br></br>
-                
+
                 <FormControl id="email" isRequired>
                     <FormLabel color="#FFFFFF">Email Address</FormLabel>
                     <Input border="1px" rounded="5px" 
                         // mt="5%" mb="5%"  
-                        margin="auto"
-                        pt="2%" pb="2%" pl="5%" pr="5%" 
+                        // margin="auto"
+                        // pt="2%" pb="2%" pl="5%" pr="5%" 
                         backgroundColor="rgba(216,216,216,0.1)" color="#bcbccc">
                     
                     </Input>
@@ -62,13 +55,15 @@ const ContactUs = () => {
                     <FormLabel color="#FFFFFF">Message</FormLabel>
                     <Input border="1px" rounded="5px" 
                         // mt="5%" mb="5%"  
-                        margin="auto"
-                        pt="2%" pb="2%" pl="5%" pr="5%" 
+                        // margin="auto"
+                        // pt="2%" pb="2%" pl="5%" pr="5%" 
                         backgroundColor="rgba(216,216,216,0.1)" color="#bcbccc">
                     </Input>
                 </FormControl>
                 <br></br>
-                <Button margin="auto" bg="#5766F1" color="#ffffff" w="84px" type="submit">Submit</Button>
+                <Box textAlign="center">
+                    <Button  bg="#5766F1" color="#ffffff" w="84px" type="submit">Submit</Button>
+                </Box>
             </form>
         </Box>
     </Box>
