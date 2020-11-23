b1313b1bf6520b5462b0e0889ccc3687dbb5de36
Skylar Wong <li.q.wong@gmail.com>
2020-11-23 14:56:59 -0600

    add option and description of problem to contact us page

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -5,7 +5,8 @@ import {
     Button,
     Input,
     FormControl,
-    Textarea
+    Textarea,
+    Flex
   } from '@chakra-ui/core'
 
 const ContactUs = () => {
@@ -26,13 +27,19 @@ const ContactUs = () => {
                 </FormControl>
                 
                 <FormControl id="phone-number" my="3%">
-                    <FormLabel color="#FFFFFF">Phone Number</FormLabel>
+                    <Flex alignItems="center">
+                        <FormLabel color="#FFFFFF">Phone Number</FormLabel>
+                        <Text fontSize="75%">(Optional)</Text>
+                    </Flex>
                     <Input border="1px" rounded="5px" backgroundColor="rgba(216,216,216,0.1)" color="#bcbccc"></Input>
                 </FormControl>
                 
                 <FormControl id="message" isRequired>
-                    <FormLabel color="#FFFFFF">Message</FormLabel>
-                    <Textarea border="1px" rounded="5px" backgroundColor="rgba(216,216,216,0.1)" color="#bcbccc"></Textarea>
+                    <Flex alignItems="center">
+                        <FormLabel color="#FFFFFF">Message</FormLabel>
+                        <Text  fontSize="75%">(Description of Problem)</Text>
+                    </Flex>
+                    <Textarea border="1px" rounded="5px" backgroundColor="rgba(216,216,216,0.1)" color="#bcbccc" placeholder="Description of Problem"></Textarea>
                 </FormControl>
                 
                 <br></br>
